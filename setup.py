ab05ae47cacfdf896c99f0ac1ca4616ca4f3cf70
podhmo <ababjam61+github@gmail.com>
2019-10-17 13:22:27 +0900

    chore: add dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-install_requires = ["typing_extensions", "magicalimport", "dictknife"]
+install_requires = ["typing_inspect", "typing_extensions", "magicalimport", "dictknife"]
 dev_requires = ["black", "flake8"]
 tests_requires = ["pytest"]
 
