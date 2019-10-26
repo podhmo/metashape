eccb05de3106c7bc26452611c51e002f997aed23
podhmo <ababjam61+github@gmail.com>
2019-10-26 10:51:29 +0900

    fix: typing drivers.openapi

--- metashape/drivers/openapi/__main__.py
+++ metashape/drivers/openapi/__main__.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
     from metashape.cli import main
-    from metashape.drivers.openapi import emit
+    from metashape.drivers.openapi.emit import emit
 
     main(emit=emit)
