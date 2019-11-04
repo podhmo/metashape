7775baedb37c66aa5e4965fa5b16166bd61e6f87
podhmo <ababjam61+github@gmail.com>
2019-11-04 18:42:05 +0900

    tests: add tests for get_walker()

---
+++ metashape/tests/test_runtime.py
@@ -0,0 +1,98 @@
+# type: ignore
+import pytest
+import dataclasses
+
+
+CODE = """\
+from __future__ import annotations
+import typing as t
+
+class A:
+    b: B
+    cs: t.List[C]
+    i: I
+
+class B:
+    d: D
+    es: t.Tuple[E]
+    i: I
+
+class C:
+    f: F
+    gs: t.Dict[G, H]
+    i: I
+
+class D:
+    name: str
+
+class E:
+    name: str
+
+class F:
+    name: str
+
+class G:
+    name: str
+
+class H:
+    name: str
+
+class I:
+    name: str
+"""
+
+
+@dataclasses.dataclass(frozen=True)
+class C:
+    aggressive: bool = False
+    recursive: bool = False
+
+
+@pytest.mark.parametrize(
+    "msg, c, input, want",
+    [
+        ("one", C(), lambda m: m.A, []),
+        ("one, aggressive=True", C(aggressive=True), lambda m: m.A, ["A"]),
+        ("one, recursive=True", C(recursive=True), lambda m: m.A, []),
+        (
+            "one, aggressive=True, recursive=True",
+            C(aggressive=True, recursive=True),
+            lambda m: m.A,
+            ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
+        ),
+        ("list", C(), lambda m: [m.A, m.B], []),
+        ("list, aggressive=True", C(aggressive=True), lambda m: [m.A, m.B], ["A", "B"]),
+        (
+            "list, aggressive=True, recursive=True",
+            C(aggressive=True, recursive=True),
+            lambda m: [m.A, m.B],
+            ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
+        ),
+        (
+            "dict, aggressive=True, recursive=True",
+            C(aggressive=True, recursive=True),
+            lambda m: m.__dict__,
+            ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
+        ),
+    ],
+)
+def test_walker(msg, c, input, want):
+    from metashape.runtime import get_walker as callFUT
+
+    def _create_module(code: str):
+        from magicalimport import import_from_physical_path
+        import tempfile
+
+        # create fake module
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".py", delete=True) as wf:
+            print("@", wf.name)
+            print(code, file=wf)
+            wf.flush()
+            return import_from_physical_path(wf.name)
+
+    m = _create_module(CODE)
+    nodes = callFUT(input(m), aggressive=c.aggressive, recursive=c.recursive).walk()
+    got = [cls.__name__ for cls in nodes]
+
+    assert len(got) == len(want)
+    assert set(got) == set(want)
