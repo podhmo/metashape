bb656b953a10ad357e44cc63407131f976b5ad86
podhmo <ababjam61+github@gmail.com>
2019-12-06 02:18:02 +0900

    fix: fix when ield.type is str

--- metashape/_dataclass.py
+++ metashape/_dataclass.py
@@ -1,4 +1,5 @@
 import typing as t
+import sys
 from collections import ChainMap
 from dataclasses import fields, Field
 from dataclasses import is_dataclass
@@ -11,7 +12,19 @@ def iterate_props(
     for field in fields(typ):  # type: Field[t.Any]
         mutable_state: t.Dict[str, t.Any] = {}
         metadata = ChainMap(mutable_state, field.metadata)
-        yield field.name, field.type, metadata
+        field_type = _evaluate_type(typ, field)
+        yield field.name, field_type, metadata
+
+
+def _evaluate_type(
+    typ: t.Type[t.Any], field: "Field[t.Any]", *, _empty: t.Dict[str, t.Any] = {}
+) -> t.Type[t.Any]:
+    field_type = field.type
+    if not isinstance(field_type, str):
+        return field_type
+    m = sys.modules[typ.__module__]
+    field_type = field.type = t.ForwardRef(field_type)._evaluate(m.__dict__, _empty)
+    return field_type
 
 
 # type assertion
