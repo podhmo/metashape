43b419cb12f535b1892c4b432b948fd2d5156cfe
podhmo <ababjam61+github@gmail.com>
2019-12-10 04:15:14 +0900

    Collect with extra parameters (#49)
    * feat: collect with extra parameters
    
    * chore: update examples

--- examples/_misc/00parse-class/main.py
+++ examples/_misc/00parse-class/main.py
@@ -1,8 +1,8 @@
 import sys
-import typing as t
 from dictknife import loading
 from magicalimport import import_symbol
 from metashape.analyze.collector import Collector, _Value
+from metashape.analyze.walker import Walker
 from metashape.runtime import get_walker
 
 
@@ -11,19 +11,18 @@ if len(sys.argv) > 1:
 else:
     cls = import_symbol("./conf.py:Toplevel", here=__file__)
 
-w = get_walker(cls)
-
 
 @Collector
-def collect(target: _Value) -> _Value:
+def collect(target: _Value, *, w: Walker) -> _Value:
     props = {}
     for name, typeinfo, metadata in w.for_type(target).walk():
         fieldname = w.resolver.metadata.resolve_name(metadata, default=name)
-        props[fieldname] = collect(getattr(target, name))
+        props[fieldname] = collect(getattr(target, name), w=w)
     return props
 
 
 d = {}
+w = get_walker(cls)
 for cls in w.walk():
-    d.update(collect(cls))
+    d.update(collect(cls, w=w))
 loading.dumpfile(d, format="yaml")
