a673089dd34618d77a5a3311bc324d2b6a8a25cb
podhmo <ababjam61+github@gmail.com>
2020-05-13 12:33:54 +0900

    For 3.7 (#55)
    * feat: support 3.7, indirect key
    
    * tests: update tests
    
    * fix: use weakref
    
    * refactor: fix for typing and lazy initialization
    
    * fix: use weakref all
    
    * chore: update input

--- metashape/name.py
+++ metashape/name.py
@@ -1,20 +1,53 @@
+from __future__ import annotations
 import typing as t
 import typing_extensions as tx
 from collections import abc
+from .langhelpers import reify
+
+if t.TYPE_CHECKING:
+    import weakref
 
 TypeT = t.TypeVar("TypeT", bound=type)
 
 
-class NameResolver:
-    def __init__(self) -> None:
-        self.pool: t.Dict[int, str] = {}  # leak?
+class _Fake:
+    def __init__(self, name: str) -> None:
+        self.name = name
 
-    def register(self, name: str, typ: TypeT) -> TypeT:
-        self.pool[id(typ)] = name
+
+_ELLIPSIS = _Fake("...")
+
+
+class NameResolver:
+    @reify
+    def pool(self) -> weakref.WeakKeyDictionary[object, str]:
+        import weakref
+
+        return weakref.WeakKeyDictionary()
+
+    @reify
+    def indirect_map(self) -> weakref.WeakValueDictionary[t.Tuple[t.Any, ...], object]:
+        import weakref
+
+        return weakref.WeakValueDictionary()
+
+    def _get_key(self, typ: TypeT) -> object:
+        if typ is ...:  # type: ignore
+            return _ELLIPSIS
         return typ
 
+    def register(self, name: str, typ: TypeT) -> TypeT:
+        k = self._get_key(typ)
+        self.pool[k] = name
+        if hasattr(typ, "__args__"):
+            self.indirect_map[self._get_indirect_key(typ)] = k
+        return typ
+
+    def _get_indirect_key(self, typ: t.Type[t.Any]) -> t.Tuple[t.Any, ...]:
+        return tuple([typ.__origin__, *sorted([str(x) for x in typ.__args__])])
+
     def resolve_maybe(self, typ: TypeT) -> t.Optional[str]:
-        name = self.pool.get(id(typ))
+        name = self.pool.get(self._get_key(typ))
         if name is not None:
             return name
 
@@ -24,6 +57,14 @@ class NameResolver:
             if "<locals>" in name:
                 name = typ.__name__
             return name
+        elif hasattr(typ, "__args__"):
+            ik = self._get_indirect_key(typ)
+            k = self.indirect_map.get(ik)
+            if k is not None:
+                name = self.pool.get(k)
+                if name is not None:
+                    self.pool[self._get_key(typ)] = name
+                    return name
         return None
 
     def resolve(self, typ: TypeT) -> str:
@@ -55,7 +96,6 @@ class NameGuesser:
     ) -> None:
         self.resolver = resolver
         self.format = formatter
-        self._cache: t.Dict[int, str] = {}  # leak
         self._aliases = _aliases or {...: "N", t.Any: "Any"}
         self._joiners = _joiners or {
             t.Any: self._join_name_for_default,
@@ -66,12 +106,19 @@ class NameGuesser:
         }
         assert t.Any in self._joiners
 
+    @reify
+    def _cache(self) -> weakref.WeakKeyDictionary[object, str]:
+        import weakref
+
+        return weakref.WeakKeyDictionary()
+
     def guess(self, typ: t.Type[t.Any]) -> str:
-        cached = self._cache.get(id(typ))
+        k = self.resolver._get_key(typ)
+        cached = self._cache.get(k)
         if cached is not None:
             return cached
         guessed = self._guess(typ)
-        self._cache[id(typ)] = guessed
+        self._cache[k] = guessed
         return guessed
 
     def _guess(self, typ: t.Type[t.Any]) -> str:
