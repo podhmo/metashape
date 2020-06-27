d4f2c31576d18ffdbb18cc15c34152cd955cba59
podhmo <ababjam61+github@gmail.com>
2020-06-28 01:29:32 +0900

    chore: add new example

---
+++ examples/openapi/16with-newtype/value.py
@@ -0,0 +1,24 @@
+from metashape.outputs.openapi import types
+
+
+class Value:
+    number_value: types.Number
+    float_value: types.Float
+    double_value: types.Double
+
+    int_value: types.Integer
+    int32_value: types.Int32
+    int64_value: types.Int64
+
+    string_value: types.String
+    date_value: types.Date
+    datetime_value: types.DateTime
+    password_value: types.Password
+    # byte_value: types.Byte
+    # binary_value: types.Binary
+    email_value: types.Email
+    uuid_value: types.UUID
+    uri_value: types.URI
+    hostname_value: types.Hostname
+    ipv4_value: types.IPV4
+    ipv6_value: types.IPV6
