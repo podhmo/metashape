aa06f70812bde42fef89e84b9e03baa25ae16a3f
podhmo <ababjam61+github@gmail.com>
2020-07-18 21:55:41 +0900

    feat: support __slots__ for Annotated (#70)

--- examples/openapi/18with-annotated/value.py
+++ examples/openapi/18with-annotated/value.py
@@ -6,5 +6,13 @@ class Description:
         self.description = description
 
 
+class Description2:
+    __slots__ = ("description",)
+
+    def __init__(self, description: str) -> None:
+        self.description = description
+
+
 class Person:
     name: Annotated[str, Description("name of person")]
+    age: Annotated[int, Description2("age of person")]
