6f85971d7945d0f43b2cfce0a79f429b82d4d808
podhmo <ababjam61+github@gmail.com>
2020-07-18 21:49:19 +0900

    Refactoring emitter (#69)
    * chore: omit result.result in openapi
    
    * fix: use typeddict
    
    * refactor: arange definition order
    
    * refactor: omit TypeInfoResolver
    
    * refactor: update emitter
    
    * refactor: add _build_property_data()
    
    * refactor: Scanner -> Builder
    
    * refactor: arrange definition order
    
    * refactor: more strict type
    
    * fix: fix type property is needed
    
    * chore: update examples

--- metashape/outputs/openapi/emit.py
+++ metashape/outputs/openapi/emit.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 import typing as t
+import typing_extensions as tx
 import logging
 import dataclasses
 from functools import partial
 from dictknife import loading
-from metashape.types import Member, _ForwardRef
+from metashape.types import Member, MetaData
 from metashape.langhelpers import make_dict, reify
 from metashape.typeinfo import TypeInfo
+from metashape.analyze.resolver import Resolver
 from metashape.analyze.walker import Walker
 from metashape.analyze.config import Config as AnalyzingConfig
 
@@ -19,43 +21,120 @@ logger = logging.getLogger(__name__)
 # TODO: conflict name
 
 
-class Context:  # TODO: rename to context?
-    @dataclasses.dataclass(frozen=False, unsafe_hash=True)
-    class State:
-        schemas: t.Dict[str, t.Any] = dataclasses.field(default_factory=make_dict)
-        refs: t.Dict[t.Type[t.Any], t.Dict[str, str]] = dataclasses.field(
-            default_factory=make_dict
-        )
+def scan(walker: Walker,) -> Context:
+    ctx = Context(walker)
+    builder = Builder(ctx, walker=walker)
 
-    @dataclasses.dataclass(frozen=False, unsafe_hash=True)
-    class Result:
-        result: t.Dict[str, t.Any] = dataclasses.field(
-            default_factory=lambda: make_dict(components=make_dict(schemas=make_dict()))
-        )
+    try:
+        for cls in walker.walk():
+            schema = builder.build_schema_data(cls)
+            ctx.register_schema(cls, schema)
+    finally:
+        ctx.config.callbacks.teardown()  # xxx:
+    return ctx
+
+
+def emit(ctx: Context, *, output: t.Optional[t.IO[str]] = None) -> None:
+    loading.dump(ctx.result, output, format=ctx.config.option.output_format)
+
+
+class Context:
+    state: Context.State
+    result: ResultDict
+    resolver: Resolver
+    config: AnalyzingConfig
 
     def __init__(self, walker: Walker) -> None:
         self.state = Context.State()
-        self.result = Context.Result()
-        self.walker = walker
+        self.result = {"components": {"schemas": {}}}
+        self.resolver = walker.resolver
         self.config = walker.config
-
-    state: Context.State
-    result: Context.Result
-    walker: Walker
-    config: AnalyzingConfig
 
     @property
     def verbose(self) -> bool:
         return self.config.option.verbose
 
+    @property
+    def strict(self) -> bool:
+        return self.config.option.strict
+
+    @dataclasses.dataclass(frozen=False, unsafe_hash=True)
+    class State:
+        schemas: t.Dict[str, SchemaDict] = dataclasses.field(default_factory=make_dict)
+        refs: t.Dict[t.Type[t.Any], RefDict] = dataclasses.field(
+            default_factory=make_dict
+        )
+
+    def register_schema(self, cls: t.Type[t.Any], schema: SchemaDict) -> None:
+        typename = self.resolver.resolve_typename(cls)
+        self.state.schemas[typename] = self.result["components"]["schemas"][
+            typename
+        ] = schema
+        self.state.refs[cls] = {"$ref": f"#/components/schemas/{typename}"}
+
+
+class ResultDict(tx.TypedDict):
+    components: ComponentDict
+
+
+class ComponentDict(tx.TypedDict):
+    schemas: t.Dict[str, SchemaDict]
+
+
+class SchemaDict(tx.TypedDict, total=False):
+    # required
+    type: detect.JSONSchemaType
+
+    # optional
+    description: str
+    default: t.Any
+    properties: t.Dict[str, t.Union[PropertyDict, RefDict, OneOfDict]]
+    required: t.List[str]
+    additionalProperties: t.Union[bool, t.Dict[str, t.Any]]
+
+
+class OneOfDict(tx.TypedDict, total=False):
+    # required
+    oneOf: t.List[t.Union[RefDict, t.Dict[str, t.Any]]]
+
+    # optional
+    discriminator: t.Dict[str, str]  # {PropertyName:str}
+
+
+RefDict = tx.TypedDict("RefDict", {"$ref": str})
+
+
+class PropertyDict(tx.TypedDict, total=False):
+    # required
+    type: detect.JSONSchemaType
+
+    # optional
+    format: str
+    default: t.Any
+    description: str
+    enum: t.Sequence[t.Any]
+
+    # only type=array
+    items: t.Union[RefDict, OneOfDict, t.Dict[str, t.Any]]
+
 
 class _Fixer:
-    ctx: Context
-
-    def __init__(self, ctx: Context) -> None:
+    def __init__(self, ctx: Context, *, discriminator_name: str) -> None:
         self.ctx = ctx
+        self.discriminator_name = discriminator_name
 
-    def fix_discriminator(self, name: str, fieldname: str) -> None:
+    def register_fix_discriminator_callback(self, info: TypeInfo) -> None:
+        resolver = self.ctx.resolver
+
+        for x in info.args:
+            if x.user_defined_type is None:
+                continue
+            name = resolver.resolve_typename(x.user_defined_type)
+            self.ctx.config.callbacks.append(
+                partial(self.fix_discriminator, name, fieldname=self.discriminator_name)
+            )
+
+    def fix_discriminator(self, name: str, *, fieldname: str) -> None:
         state = self.ctx.state
         schema = state.schemas.get(name)
         if schema is None:
@@ -72,69 +151,111 @@ class _Fixer:
             schema["required"] = [fieldname]
 
 
-class Scanner:
+class Builder:
     DISCRIMINATOR_FIELD = "$type"
 
-    ctx: Context
-
-    def __init__(self, ctx: Context) -> None:
+    def __init__(self, ctx: Context, *, walker: Walker) -> None:
         self.ctx = ctx
+        self.walker = walker
 
     @reify
     def fixer(self) -> _Fixer:
-        return _Fixer(self.ctx)
+        return _Fixer(self.ctx, discriminator_name=self.DISCRIMINATOR_FIELD)
 
-    def _build_ref_data(
-        self, field_type: t.Union[t.Type[t.Any], _ForwardRef]
-    ) -> t.Dict[str, t.Any]:
-        resolver = self.ctx.walker.resolver
+    def build_ref_data(self, field_type: Member) -> RefDict:
+        ref_data = self.ctx.state.refs.get(field_type)
+        if ref_data is not None:
+            return ref_data
+
+        resolver = self.ctx.resolver
+        self.walker.append(field_type)
         return {
             "$ref": f"#/components/schemas/{resolver.resolve_typename(field_type)}"
         }  # todo: lazy
 
-    def _build_one_of_data(self, info: TypeInfo) -> t.Dict[str, t.Any]:
-        resolver = self.ctx.walker.resolver
-        cfg = self.ctx.walker.config
-        candidates: t.List[t.Dict[str, t.Any]] = []
+    def build_one_of_data(self, info: TypeInfo) -> OneOfDict:
+        candidates: t.List[t.Union[RefDict, t.Dict[str, detect.JSONSchemaType]]] = []
         need_discriminator = True
 
         for x in info.args:
-            user_defined_type = x.user_defined_type
-            if user_defined_type is None:
+            if x.user_defined_type is None:
                 need_discriminator = False
                 candidates.append({"type": detect.schema_type(x)})
             else:
-                candidates.append(self._build_ref_data(user_defined_type))
-        prop: t.Dict[str, t.Any] = {"oneOf": candidates}  # todo: discriminator
+                candidates.append(self.build_ref_data(x.user_defined_type))
 
+        prop: OneOfDict = {"oneOf": candidates}
         if need_discriminator:
             prop["discriminator"] = {"propertyName": self.DISCRIMINATOR_FIELD}
-            # update schema
-            for x in info.args:
-                user_defined_type = x.user_defined_type
-                if user_defined_type is None:
-                    continue
-                cfg.callbacks.append(
-                    partial(
-                        self.fixer.fix_discriminator,
-                        resolver.resolve_typename(user_defined_type),
-                        self.DISCRIMINATOR_FIELD,
-                    )
-                )
+            self.fixer.register_fix_discriminator_callback(info)
         return prop
 
-    def scan(self, cls: Member) -> None:
+    def build_property_data(
+        self, info: TypeInfo, *, metadata: MetaData
+    ) -> t.Union[PropertyDict, RefDict]:
+        resolver = self.ctx.resolver
+        metadata_resolver = resolver.metadata
+
+        # TODO: self recursion check (warning)
+        if resolver.is_member(info.type_) and resolver.resolve_typename(info.type_):
+            return self.build_ref_data(info.type_)
+
+        prop: PropertyDict = make_dict()
+        if info.is_combined:
+            prop.update(self.build_one_of_data(info))  # type: ignore
+        else:
+            prop["type"] = detect.schema_type(info)
+            enum = detect.enum(info)
+            if enum:
+                prop["enum"] = enum
+
+        # description
+        if metadata.get("description"):
+            prop["description"] = metadata_resolver.resolve_doc(
+                info.type_, verbose=True, value=metadata["description"]
+            )
+
+        # default
+        if metadata_resolver.has_default(metadata):
+            prop["default"] = metadata_resolver.resolve_default(metadata)
+
+        # inject extra data
+        metadata_resolver.fill_extra_metadata(
+            prop, metadata, name="openapi"  # type:ignore
+        )
+
+        if prop.get("type") == "array":  # todo: simplify with recursion
+            assert len(info.args) == 1
+            first = info.args[0]
+            if first.is_combined and first.is_container:
+                prop["items"] = self.build_one_of_data(first)
+            elif first.user_defined_type is None:
+                prop["items"] = {"type": detect.schema_type(first)}
+            else:
+                if first.user_defined_type is not None:
+                    prop["items"] = self.build_ref_data(first.user_defined_type)
+
+        if info.is_newtype:
+            if info.user_defined_type is not None:
+                if prop.get("type") == "object":
+                    return self.build_ref_data(info.user_defined_type)
+            else:
+                if hasattr(info.supertypes[0], "__name__"):
+                    prop["format"] = resolver.resolve_typeformat(info)
+
+        return prop
+
+    def build_schema_data(self, cls: Member) -> SchemaDict:
         ctx = self.ctx
-        walker = self.ctx.walker
-        resolver = self.ctx.walker.resolver
-        cfg = self.ctx.config
-        typename = resolver.resolve_typename(cls)
+        walker = self.walker
+        resolver = self.ctx.resolver
+        metadata_resolver = resolver.metadata
 
         required: t.List[str] = []
         properties: t.Dict[str, t.Any] = make_dict()
-        description: str = resolver.metadata.resolve_doc(cls, verbose=ctx.verbose)
+        description: str = metadata_resolver.resolve_doc(cls, verbose=ctx.verbose)
 
-        schema: t.Dict[str, t.Any] = make_dict(
+        schema: SchemaDict = make_dict(
             type="object",
             properties=properties,
             required=required,
@@ -142,78 +263,17 @@ class Scanner:
         )
 
         for field_name, info, metadata in walker.walk_fields(cls):
-            field_name = resolver.metadata.resolve_name(metadata, default=field_name)
+            field_name = metadata_resolver.resolve_name(metadata, default=field_name)
             if not info.is_optional:
                 required.append(field_name)
 
-            # TODO: self recursion check (warning)
-            if resolver.is_member(info.type_) and resolver.resolve_typename(info.type_):
-                walker.append(info.type_)
+            properties[field_name] = self.build_property_data(info, metadata=metadata)
 
-                properties[field_name] = self._build_ref_data(info.type_)
-                continue
-
-            if info.is_combined:
-                properties[field_name] = prop = self._build_one_of_data(info)
-            else:
-                prop = properties[field_name] = {"type": detect.schema_type(info)}
-                enum = detect.enum(info)
-                if enum:
-                    prop["enum"] = enum
-
-            # description
-            if metadata.get("description"):
-                prop["description"] = metadata["description"]
-
-            # default
-            if resolver.metadata.has_default(metadata):
-                prop["default"] = resolver.metadata.resolve_default(metadata)
-            resolver.metadata.fill_extra_metadata(prop, metadata, name="openapi")
-
-            if prop.get("type") == "array":  # todo: simplify with recursion
-                assert len(info.args) == 1
-                first = info.args[0]
-                if first.is_combined and first.is_container:
-                    prop["items"] = self._build_one_of_data(first)
-                elif first.user_defined_type is None:
-                    prop["items"] = detect.schema_type(first)
-                else:
-                    if first.user_defined_type is not None:
-                        prop["items"] = self._build_ref_data(first.user_defined_type)
-
-            if info.is_newtype:
-                if info.user_defined_type is not None:
-                    if prop.get("type") == "object":
-                        prop.pop("type")
-                        prop.update(ctx.state.refs[info.user_defined_type])
-                else:
-                    if hasattr(info.supertypes[0], "__name__"):
-                        prop["format"] = info.supertypes[0].__name__.replace("_", "-")
-
+        # simplify
         if len(required) <= 0:
             schema.pop("required")
         if not description:
             schema.pop("description")
-        if cfg.option.strict and "additionalProperties" not in schema:
+        if ctx.strict and "additionalProperties" not in schema:
             schema["additionalProperties"] = False
-
-        ctx.state.schemas[typename] = ctx.result.result["components"]["schemas"][
-            typename
-        ] = schema
-        ctx.state.refs[cls] = {"$ref": f"#/components/schemas/{typename}"}
-
-
-def scan(walker: Walker,) -> Context:
-    ctx = Context(walker)
-    scanner = Scanner(ctx)
-
-    try:
-        for cls in walker.walk():
-            scanner.scan(cls)
-    finally:
-        ctx.config.callbacks.teardown()  # xxx:
-    return ctx
-
-
-def emit(ctx: Context, *, output: t.Optional[t.IO[str]] = None) -> None:
-    loading.dump(ctx.result.result, output, format=ctx.config.option.output_format)
+        return schema
--- metashape/types.py
+++ metashape/types.py
@@ -1,17 +1,9 @@
+from __future__ import annotations
 import typing as t
 import typing_extensions as tx
 from .constants import ID  # noqa F401
 
 T = t.TypeVar("T")
-
-Kind = tx.Literal["object", "enum", "ignore"]
-MetaData = t.MutableMapping[str, t.Any]
-
-# TODO: more strict typing
-Member = t.Type[t.Any]
-IsMemberFunc = t.Callable[[t.Type[t.Any]], bool]
-GuessMemberFunc = t.Callable[[t.Type[t.Any]], t.Optional[Kind]]
-EmitFunc = t.Callable[..., None]
 
 
 class IteratePropsFunc(tx.Protocol):
@@ -26,3 +18,15 @@ class _ForwardRef(tx.Protocol):
     @property
     def __forward_arg__(self) -> str:
         ...
+
+
+# TODO: more strict typing
+Kind = tx.Literal["object", "enum", "ignore"]
+MetaData = t.MutableMapping[str, t.Any]
+
+Member = t.Type[t.Any]
+MemberOrRef = t.Union[t.Type[t.Any], _ForwardRef]
+IsMemberFunc = t.Callable[[t.Type[t.Any]], bool]
+GuessMemberFunc = t.Callable[[t.Type[t.Any]], t.Optional[Kind]]
+
+EmitFunc = t.Callable[..., None]
