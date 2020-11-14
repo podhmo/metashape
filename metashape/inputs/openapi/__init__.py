4cc5a7c84796a12a9b90435cba73f1130df1cd7d
podhmo <ababjam61+github@gmail.com>
2020-11-15 01:16:53 +0900

    feat: emit only schema

--- metashape/inputs/openapi/__init__.py
+++ metashape/inputs/openapi/__init__.py
@@ -30,6 +30,91 @@ class Resolver:
         self.fulldata = fulldata
         self._accessor = dictknife.Accessor()  # todo: rename
 
+    def resolve_python_type(self, d: AnyDict, *, name: str,) -> t.Type[t.Any]:
+        type_hints = {}
+        for field_name, field in d["properties"].items():
+            type_hints[field_name] = str  # TODO: guess type
+        return type(name, (), {"__annotations__": type_hints})
+
+    ##
+
+    def has_ref(self, d: AnyDict) -> bool:
+        return "$ref" in d
+
+    def has_allof(self, d: AnyDict) -> bool:
+        return "allOf" in d
+
+    def has_schema(
+        self, d: AnyDict, cand: t.Tuple[str, ...] = ("object",), fullscan: bool = True,
+    ) -> bool:
+        typ = d.get("type", None)
+        if typ in cand:
+            return True
+        if "properties" in d:
+            return True
+        if self.has_allof(d):
+            return True
+        if not self.has_ref(d):
+            return False
+        if not fullscan:
+            return False
+
+        # TODO: cache?
+        _, definition = self.resolve_ref_definition(d)
+        return self.has_schema(definition, fullscan=False)
+
+    ##
+    def resolve_ref_definition(
+        self,
+        d: t.Dict[str, t.Any],
+        name: t.Optional[str] = None,
+        i: int = 0,
+        level: int = -1,
+    ) -> t.Tuple[str, t.Dict[str, t.Any]]:
+        # return schema_name, definition_dict
+        # todo: support quoted "/"
+        # on array
+        if "items" in d:
+            definition = d
+            name, _ = self.resolve_ref_definition(
+                d["items"], name=name, i=i, level=level + 1
+            )  # xxx
+            return name, definition
+
+        if "$ref" not in d:
+            return self.resolve_schema_name(name), d
+        if level == 0:
+            return self.resolve_schema_name(name), d
+
+        logger.debug("    resolve: %sref=%r", "  " * i, d["$ref"])
+
+        path = d["$ref"][len("#/") :].split("/")
+        name = path[-1]
+
+        parent = self._accessor.maybe_access_container(path)
+        if parent is None:
+            logger.warning("%r is not found", d["$ref"])
+            return self.resolve_schema_name(name), d
+        ref_name, definition = self.resolve_ref_definition(
+            parent[name], name=name, i=i + 1, level=level - 1
+        )
+
+        # import for separated output
+        # if X_MARSHMALLOW_INLINE not in definition:
+        #     if c is not None and (
+        #         "properties" in definition
+        #         or (
+        #             (
+        #                 "additionalProperties" in definition
+        #                 or "items" in definition
+        #                 or "allOf" in definition
+        #             )
+        #             and self.has_schema(fulldata, definition)
+        #         )
+        #     ):
+        #         c.relative_import_from_lazy(ref_name)
+        return ref_name, definition
+
 
 class Accessor:
     def __init__(self, resolver: Resolver):
@@ -52,7 +137,6 @@ class Accessor:
 
     def extract_python_type(self, name: str, d: AnyDict) -> t.Type[t.Any]:
         metadata_dict = self._extract_metadata_dict_pre_properties(d)
-
         type_hints = {}
         for field_name, field in d["properties"].items():
             metadata = metadata_dict[field_name]
@@ -126,6 +210,10 @@ def main(d: AnyDict) -> None:
     resolver = Resolver(d)
     a = Accessor(resolver)
     for name, sd in a.schemas(d):
+        if not a.resolver.has_schema(sd):
+            logger.debug("skip schema %s", name)
+            continue
+
         # TODO: normalize
         ctx.types[name] = a.extract_python_type(name, sd)
 
