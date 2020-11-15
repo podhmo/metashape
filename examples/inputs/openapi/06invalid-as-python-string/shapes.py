2370e18f9aea46704ba15325524ff13197e0dd5d
podhmo <ababjam61+github@gmail.com>
2020-11-16 01:07:46 +0900

    chore: update examples

---
+++ examples/inputs/openapi/06invalid-as-python-string/shapes.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+import typing
+
+
+# original is 💣
+class _invalid_:
+    _invalid_: typing.Optional[_invalid_]  # original is 💣
+    _invalid_G1: typing.Optional[str]  # original is ⽝
+    n100: typing.Optional[str]  # original is 100
+    n1234: typing.Optional[str]  # original is 1234
+    n1: typing.Optional[str]  # original is 1
+    _invalid__1: typing.Optional[str]  # original is -1
+    n8ball: typing.Optional[str]  # original is 8ball
