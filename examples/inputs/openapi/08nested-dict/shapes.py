b89f423f0aae29e534fa3ca29682c0123b0f55f4
podhmo <ababjam61+github@gmail.com>
2020-11-17 01:29:14 +0900

    chore: update examples

--- examples/inputs/openapi/08nested-dict/shapes.py
+++ examples/inputs/openapi/08nested-dict/shapes.py
@@ -3,8 +3,13 @@ import typing
 
 
 class S:
-    x: typing.Optional[typing.Dict[str, S]]
-    y: typing.Optional[typing.Dict[str, S]]
-    y2: typing.Optional[typing.Dict[str, S]]
-    z: typing.Optional[typing.Dict[str, S]]
-    z2: typing.Optional[typing.Dict[str, typing.Dict[str, S]]]
+    x: typing.Optional[typing.Dict[str, str]]
+    x2: typing.Optional[typing.Dict[str, typing.Dict[str, str]]]
+    x3: typing.Optional[typing.Dict[str, typing.Dict[str, typing.Dict[str, str]]]]
+    y: typing.Optional[typing.Dict[str, str]]
+    y2: typing.Optional[typing.Dict[str, str]]
+    y3: typing.Optional[typing.Dict[str, typing.Dict[str, str]]]
+    z: typing.Optional[typing.Dict[str, str]]
+    z2: typing.Optional[typing.Dict[str, typing.Dict[str, str]]]
+    z3: typing.Optional[typing.Dict[str, typing.Dict[str, typing.Dict[str, str]]]]
+    z4: typing.Optional[typing.Dict[str, typing.Dict[str, typing.Dict[str, typing.Dict[str, str]]]]]
