f0f665b76e7ad9630f721a340ec4c97250023b4b
podhmo <ababjam61+github@gmail.com>
2022-06-06 14:25:04 +0900

    refactor: use property

--- metashape/typeinfo2.py
+++ metashape/typeinfo2.py
@@ -6,22 +6,61 @@ from logging import getLogger
 logger = getLogger(__name__)
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(slots=True, frozen=True)
 class TypeInfo:
-    # __slots__ = ("name", "underlying", "is_primitive", "is_container", "is_optional")
+    """
+    str                  -> is_primitive=True
+    NewType("Name", str) -> is_primitive=True, is_newtype=True
+    Literal["x","y","z"] -> is_primitive=True, is_newtype=True?, is_enum=True
+    List[str]            -> is_primitive=False, is_container=True, args=[TypeInfo[str]], python_container_type=list
+    Union[int, str]      -> is_primitive=False, is_container=True?, args=[TypeInfo[int], TypeInfo[str]]
+    Union[str, int]      -> is_primitive=False, is_container=True?, args=[TypeInfo[int], TypeInfo[str]]  # sorted
+
+    class A: pass
+    class B: pass
+
+    A                    -> is_primitive=False, user_defined_types=[TypeInfo[A]]
+    NewType("A", B)      -> is_primitive=False, user_defined_types=[TypeInfo[A]], is_newtype=True
+    List[A]              -> is_primitive=False, user_defined_types=[TypeInfo[A]], is_container=True, python_container_type=list
+    Dict[A, B]           -> is_primitive=False, user_defined_types=[TypeInfo[A], TypeInfo[B]], is_container=True, python_container_type=dict
+    """
 
     name: str
     python_type: t.Type[t.Any]
-
     args: t.List[TypeInfo]
-    underlying: TypeInfo  # for example, NewType[str] is passed, str is underlying type
+    user_defined_types: t.List[TypeInfo]  # todo: renmae
 
-    is_primitive: bool  # e.g. str, int is primitive
-    is_container: bool = False  # array and dict are container (Union has args but not container?)
-    is_optional: bool = False  # Nullable is is_optiiional
+    _underlying: t.Optional[
+        TypeInfo
+    ] = None  # for example, NewType[str] is passed, str is underlying type
+    python_container_type: t.Optional[t.Type[t.Any]] = None
+    is_optional: bool = False  # Nullable
 
-    is_enum: bool = False  #
-    is_newtype: bool = False
+    @property
+    def underlying(self) -> TypeInfo:
+        return self._underlying or self
+
+    @property
+    def is_primitive(self) -> bool:
+        # e.g. str, int is primitive
+        return not (self._underlying is not None or len(self.user_defined_types) > 0)
+
+    @property
+    def is_enum(self) -> bool:
+        return len(self.enum_values) > 0
+
+    @property
+    def is_newtype(self) -> bool:
+        return self._underlying is not None
+
+    @property
+    def is_container(self) -> bool:
+        # array and dict are container (Union has args but not container?)
+        return self.python_container_class is not None
+
+    @property
+    def has_user_defined_types(self) -> bool:
+        return len(self.user_defined_types) > 0
 
     def __str__(self):
         newtype_suffix = "@" + self.underlying.name if self.is_newtype else ""
@@ -37,9 +76,6 @@ _primitives_types: t.Set[t.Type[t.Any]] = set(
     [str, int, float, bool, str, bytes, dict, list, t.Any]
 )
 
-_zero = TypeInfo(name="*zero*", python_type=TypeInfo, args=[], underlying=None, is_primitive=True)  # type: ignore
-_zero = dataclasses.replace(_zero, underlying=_zero)
-
 
 # TODO: t.List[str]
 # TODO: user defined class
@@ -50,7 +86,7 @@ _zero = dataclasses.replace(_zero, underlying=_zero)
 def typeinfo(typ: t.Type[t.Any], _toplevel: bool = True) -> TypeInfo:
     if _toplevel:
         logger.debug("-------------------- typeinfo: %r --------------------", typ)
-    ti = _typeinfo(typ, raw=typ, is_optional=False, lv=0)
+    ti = _typeinfo(typ, is_optional=False, lv=0)
     if _toplevel:
         logger.debug("-- %r --", ti)
     return ti
@@ -59,10 +95,8 @@ def typeinfo(typ: t.Type[t.Any], _toplevel: bool = True) -> TypeInfo:
 def _typeinfo(
     typ: t.Type[t.Any],
     *,
-    raw: t.Type[t.Any],
-    underlying: TypeInfo = _zero,
+    underlying: t.Optional[TypeInfo] = None,
     is_optional: bool = False,
-    is_newtype: bool = False,
     lv: int = 0,
 ) -> TypeInfo:
     logger.debug("\t%s%r", "  " * lv, typ)
@@ -70,57 +104,52 @@ def _typeinfo(
     origin = getattr(typ, "__origin__", None)
 
     supertypes = []
+    raw_type = typ
     while hasattr(typ, "__supertype__"):
         supertypes.append(typ)  # todo: fullname?
         typ = typ.__supertype__
-        is_newtype = True
+    is_newtype = raw_type != typ
 
-    if origin is None:
+    if origin is None:  # for NewType
         python_type = typ
-        underlying = _zero
         if is_newtype:
-            python_type = raw
+            python_type = raw_type
             underlying = typeinfo(typ, _toplevel=False)
-        return dataclasses.replace(
-            _zero,
+        return TypeInfo(
             name=python_type.__name__,
-            underlying=underlying,
             python_type=python_type,
-            is_newtype=is_newtype,
+            args=[],
+            user_defined_types=[],
+            _underlying=underlying,
             is_optional=is_optional,
         )
-
-    if origin == t.Union:
+    elif origin == t.Union:
         if len(args) == 2:
             if args[0] == _nonetype:
-                typ = args[1]
                 return _typeinfo(
-                    typ,
-                    raw=typ,
-                    is_newtype=is_newtype,
+                    args[1],
                     is_optional=True,
                     lv=lv + 1,
                 )
             elif args[1] == _nonetype:
-                typ = args[0]
                 return _typeinfo(
-                    typ,
-                    raw=typ,
-                    is_newtype=is_newtype,
+                    args[0],
                     is_optional=True,
                     lv=lv + 1,
                 )
-        is_optional = _nonetype in args
-        if is_optional:
-            args = [x for x in args if x != _nonetype]
-        typ = t.Union[tuple(sorted(args, key=str))]  # type: ignore
+
+        new_args = sorted([x for x in args if x != _nonetype], key=str)
+        typ = t.Union[tuple(new_args)]  # type: ignore
+        args_info_list = [typeinfo(x, _toplevel=False) for x in new_args]
         return TypeInfo(
             name=str(typ),
             python_type=typ,
-            underlying=_zero,  # xxx?
-            args=[typeinfo(x, _toplevel=False) for x in args],
-            is_optional=is_optional,
-            is_primitive=False,
+            python_container_type=t.Union,
+            args=args_info_list,
+            user_defined_types=[
+                ut for x in args_info_list for ut in x.user_defined_types
+            ],  # TODO: dedup
+            is_optional=len(args) != len(new_args),
         )
 
     raise NotImplementedError("never")
